3a512b7e8c77ae0eba2ff6c1d531d86d8163bcc3
Jun <airport.0930@gmail.com>
2026-06-04 23:59:55 +0800

    Initial Commit

---
+++ TokenMaxing/TokenMaxingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenMaxingApp.swift
+//  TokenMaxing
+//
+//  Created by zp on 2026/6/4.
+//
+
+import SwiftUI
+
+@main
+struct TokenMaxingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
